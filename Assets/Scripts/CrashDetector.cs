a124fc4406c61ec625494337c0fc5fecc8a079b1
Hüseyin Demirci <87519251+hsyndmrc@users.noreply.github.com>
2023-10-28 10:29:33 +0300

    CrashDeterctor Update

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.tag == "Ground")
+        {
+            Debug.Log("Ouch, hit my head");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
