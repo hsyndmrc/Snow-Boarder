eba00a2d2f72fcef0020cc0a9896ce320144dc67
Hüseyin Demirci <87519251+hsyndmrc@users.noreply.github.com>
2023-10-30 10:26:18 +0300

    Update PlayerController Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,19 +9,27 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float torqueAmount = 1f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
+
     void Update()
+    {
+        RotatePlayer();
+    }
+
+
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(torqueAmount);
         }
-
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
