e298e4f3150600c67c83232fec6293a17bd672fe
Hüseyin Demirci <87519251+hsyndmrc@users.noreply.github.com>
2023-10-30 11:50:58 +0300

    Main character Updated
    To barry(Main character) added audio source

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -15,6 +15,7 @@ public class FinishLine : MonoBehaviour
         {
             finishEffect.Play();
             Invoke("ReloadScene", reloadSceneDelay);
+            GetComponent<AudioSource>().Play();
         }
     }
 
