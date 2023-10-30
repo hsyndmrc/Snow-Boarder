7cd9e3a2ff87d80f05224f980c58261170f11413
Hüseyin Demirci <87519251+hsyndmrc@users.noreply.github.com>
2023-10-30 10:16:35 +0300

    PlayerController Script Updated

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,13 +10,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float torqueAmount = 1f;
     Rigidbody2D rb2d;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
