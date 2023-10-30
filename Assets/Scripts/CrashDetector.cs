4a41812e0d57f3208ad16643d28fb4431e1bd828
Hüseyin Demirci <87519251+hsyndmrc@users.noreply.github.com>
2023-10-30 11:59:06 +0300

    CrashDetector Script Updated

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -9,11 +9,13 @@ public class CrashDetector : MonoBehaviour
 
     [SerializeField] float reloadSceneDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", reloadSceneDelay);
         }
     }
